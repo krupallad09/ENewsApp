cabfb622991008c891c18c879a3d30fa2240faeb
krupallad09 <krupallad032@gmail.com>
2021-08-05 13:11:39 +0530

    ENewsApp

---
+++ app/src/main/java/com/example/android/enews/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.android.enews
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), NewsItemClicked {
+
+    private lateinit var mAdapter: NewsListAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        recycleView.layoutManager = LinearLayoutManager(this)
+        fetchData()
+        mAdapter = NewsListAdapter(this)
+        recycleView.adapter = mAdapter
+    }
+
+    private fun fetchData() {
+        val url =
+            "https://newsapi.org/v2/top-headlines?country=in&apiKey=9611fa2ce7a447ab9a75f99c4085363a"
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET,
+            url,
+            null,
+            Response.Listener {
+                val newsJsonArray = it.getJSONArray("articles")
+                val newsArray = ArrayList<News>()
+                for (i in 0 until newsJsonArray.length()) {
+                    val newsJSONObject = newsJsonArray.getJSONObject(i)
+                    val news = News(
+                        newsJSONObject.getString("title"),
+                        newsJSONObject.getString("author"),
+                        newsJSONObject.getString("url"),
+                        newsJSONObject.getString("urlToImage")
+                    )
+                    newsArray.add(news)
+                }
+
+                mAdapter.updateNews(newsArray)
+            },
+            Response.ErrorListener {
+
+            }
+        )
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
+    }
+
+    override fun onItemClicked(item: News) {
+
+    }
+}
---
+++ app/src/main/java/com/example/android/enews/News.kt
@@ -0,0 +1,8 @@
+package com.example.android.enews
+
+data class News (
+        val title : String,
+        val author: String,
+        val url: String,
+        val imageUrl: String
+        )
